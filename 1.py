f698af4a6e2e4ad41dd88b31b3f910acd0f24ac4
kongguhuiyin <893671783@qq.com>
2022-03-18 21:59:49 +0800

    init

---
+++ 1.py
@@ -0,0 +1,12 @@
+import numpy
+List1 = numpy.full((10),0)
+List1[4] = 1
+print(List1)
+
+List2 = numpy.arange(10,50)
+print(List2)
+
+List3 = numpy.arange(9)
+List3 = numpy.reshape(List3, (3,3))
+print(List3)
+
